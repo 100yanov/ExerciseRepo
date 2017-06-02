3c951b5bfb8b15753044606c1e448d1a15409b5a
Stucky <stoyan.v.stoyanov86@gmail.com>
2017-06-02 22:07:15 +0300

    adding comments to \ConditionalStatementsandLoops\0.3ChooseDrink2.0\Program.cs in main branch

--- ConditionalStatementsandLoops/0.3ChooseDrink2.0/Program.cs
+++ ConditionalStatementsandLoops/0.3ChooseDrink2.0/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//!!!!!!!!!!adding another line, to master branch!!!!!!!!!
 
 namespace _0._3ChooseDrink2._0
 {
